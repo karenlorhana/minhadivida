d56a86eb34aea8c55245e253c5b6dd36c01bbf80
karenlorhana <43828995+karenlorhana@users.noreply.github.com>
2018-11-14 17:50:35 -0300

    página inicial - funcionando

---
+++ inicio.php
@@ -0,0 +1,97 @@
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>PÁGINA INICIAL</title>
+
+        <!-- Bootstrap core CSS-->
+        <link href="https://blackrockdigital.github.io/startbootstrap-sb-admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom fonts for this template-->
+        <link href="https://blackrockdigital.github.io/startbootstrap-sb-admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+        <!-- Custom styles for this template-->
+        <link href="https://blackrockdigital.github.io/startbootstrap-sb-admin/css/sb-admin.css" rel="stylesheet">
+        <style type="text/css">
+            body{
+               background-color:#DCDCDC;
+               padding-top:0px;
+               padding-bottom:50px;
+            }
+            center{
+                padding-right:25px;
+                padding-left:25px;
+                
+            }
+         
+
+        </style>
+    </head>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="inicio.php">Início</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="inserirgastos.php">Cadastrar<span class="sr-only">(current)</span></a>
+                </li>
+                
+                <li class="nav-item dropdown">
+                <li class="nav-item">
+                    <a class="nav-link" href="relatorio.php">Relatório</a>
+                </li>
+               <li class="nav-item">
+                    <a class="nav-link" href="index.php">Exibir Registros Totais</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+    
+   
+
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        
+      
+    </div>
+        
+    <div class="container">
+      <header class="blog-header py-3">
+        
+      </header>
+
+        <br><br>
+
+        <div class="row mb-2">
+        <div class="col-md-6">
+          <div class="card flex-md-row mb-4 shadow-sm h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+              <strong class="d-inline-block mb-2 text-success"></strong>
+              <h1 class="mb-0">
+                <a class="text-dark" >Controle suas finanças com Cashness.</a>
+              </h1>
+              <br>
+              <h2 class="card-text mb-auto">“O dinheiro, assim como as emoções, é algo que você precisa controlar para manter sua vida no caminho certo”
+</h2>
+              
+            </div>
+          
+          </div>
+        </div>
+      </div>
+     
+    
+    </div>
+
+    <main role="main" class="container">
+      <div class="row">
+          <div class="col-md-8 blog-main"></div>
