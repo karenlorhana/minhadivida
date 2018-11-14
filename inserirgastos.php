105719258c8f0972d771dff325371b24a26c4097
karenlorhana <43828995+karenlorhana@users.noreply.github.com>
2018-11-14 17:51:03 -0300

    inserir gastos -  finalizado

--- inserirgastos.php
+++ inserirgastos.php
@@ -1,33 +1,62 @@
 <html lang="en">
 
-  <head>
+    <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-    <title>INSERIR GASTOS</title>
+        <title>PÁGINA INICIAL</title>
 
-    <!-- Bootstrap core CSS-->
-    <link href="https://blackrockdigital.github.io/startbootstrap-sb-admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap core CSS-->
+        <link href="https://blackrockdigital.github.io/startbootstrap-sb-admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom fonts for this template-->
-    <link href="https://blackrockdigital.github.io/startbootstrap-sb-admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <!-- Custom fonts for this template-->
+        <link href="https://blackrockdigital.github.io/startbootstrap-sb-admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
-    <!-- Custom styles for this template-->
-    <link href="https://blackrockdigital.github.io/startbootstrap-sb-admin/css/sb-admin.css" rel="stylesheet">
-<style type="text/css">
+        <!-- Custom styles for this template-->
+        <link href="https://blackrockdigital.github.io/startbootstrap-sb-admin/css/sb-admin.css" rel="stylesheet">
+        <style type="text/css">
             body{
-                background-image:url(https://www.xmple.com/wallpaper/blue-grid-graph-paper-grey-1920x1080-c2-4682b4-778899-l2-1-5-a-15-f-20.svg);
-                padding-top:50px;
+               background-color:#DCDCDC;
+               padding-top:0px;
+               padding-bottom:50px;
+            }
+            center{
                 padding-right:25px;
                 padding-left:25px;
-                padding-bottom:50px;
+                
             }
+         
+
         </style>
-  </head>
+    </head>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="inicio.php">Início</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="inserirgastos.php">Cadastrar<span class="sr-only">(current)</span></a>
+                </li>
+                
+                <li class="nav-item dropdown">
+                <li class="nav-item">
+                    <a class="nav-link" href="relatorio.php">Relatório</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Exibir Registros Totais</a>
+                </li>
+
+            </ul>
+            
+        </div>
+    </nav>
 
         <form role="form"  action="indexS.php" method="POST">
 
